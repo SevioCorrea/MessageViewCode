00f86bfd0b9a9ce09f6cdeaea2869fb6d42b09c7
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2023-02-22 10:43:38 -0300

    HomeScreen

---
+++ MessageViewCode/Home/Screen/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  MessageViewCode
+//
+//  Created by Sévio Basilio Corrêa on 22/02/23.
+//
+
+import UIKit
+
+class HomeScreen: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
